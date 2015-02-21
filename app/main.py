c87c822c9371913b89e06a7181ab800974a392d6
Chris Kelly <cpjkelly@gmail.com>
2015-02-21 13:02:04 -0800

    add moves deciding

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 import bottle
 import json
 
+game = {}
 
 @bottle.get('/')
 def index():
@@ -14,11 +15,12 @@ def index():
 @bottle.post('/start')
 def start():
     data = bottle.request.json
+    game = data
 
     return json.dumps({
         'name': 'Dem Franchize Boyz',
         'color': '#00ff00',
-        'head_url': 'https://glacial-escarpment-3430.herokuapp.com/',
+        'head_url': 'https://38.media.tumblr.com/tumblr_lzkctzyiTv1qfea58o1_500.gif',
         'taunt': 'My life for Auir!'
     })
 
@@ -28,8 +30,8 @@ def move():
     data = bottle.request.json
 
     return json.dumps({
-        'move': 'left',
-        'taunt': ''
+        'move': move_response(),
+        'taunt': 'die!'
     })
 
 
@@ -39,8 +41,29 @@ def end():
 
     return json.dumps({})
 
-def move_response
-    pass
+def move_response():
+    moves = {'left': test_left,
+            'right': test_right,
+            'up': test_up,
+            'down': test_down
+            }
+    best_move =  sorted(moves.items(), key = lambda t: t[1], reverse=True)[0][0]
+    return best_move
+
+
+def test_left():
+    return 100
+
+def test_right():
+    return 0
+
+def test_down():
+    return 0
+
+def test_up():
+    return 0
+
+
 
 # Expose WSGI app
 application = bottle.default_app()
